fad398c75710e172fafcdd213669113f85de68dd
SACHIN KOLI <132149727+Sachinkoligit@users.noreply.github.com>
2025-11-03 18:01:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://fastidious-beijinho-fc2ca1.netlify.app/",
+    origin: "https://fastidious-beijinho-fc2ca1.netlify.app",
     credentials: true,
   })
 );
