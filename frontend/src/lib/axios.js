09a7c81cacde305451222ba0accfd836bf94edeb
SACHIN KOLI <132149727+Sachinkoligit@users.noreply.github.com>
2025-11-03 17:41:04 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://bookly-app-vkj1.onrender.com/api",
   withCredentials: true,
 });
